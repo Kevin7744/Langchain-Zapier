0861d7223d782a911add38ba78cf340feaa32364
Kevin7744 <arapbiisubmissions@gmail.com>
2024-04-20 19:53:05 +0300

    update

--- main.py
+++ main.py
@@ -1,23 +1,27 @@
 from langchain.agents import AgentType, initialize_agent
 from langchain_community.agent_toolkits import ZapierToolkit
+# from langchain_community.agent_toolkits.zapier.toolkit import ZapierToolkit
 from langchain_community.utilities.zapier import ZapierNLAWrapper
 from langchain_openai import OpenAI
-import os
+from os import environ
 
 # get from https://platform.openai.com/
-os.environ["OPENAI_API_KEY"] = os.environ.get("OPENAI_API_KEY", "")
+openai_api_key = environ.get("OPENAI_API_KEY")
+
 
 # get from https://nla.zapier.com/docs/authentication/ after logging in):
-os.environ["ZAPIER_NLA_API_KEY"] = os.environ.get("ZAPIER_NLA_API_KEY", "")
+zapier_api_key = environ.get("ZAPIER_NLA_API_KEY")
 
-llm = OpenAI(temperature=0)
-zapier = ZapierNLAWrapper()
+llm = OpenAI(temperature=0, openai_api_key=openai_api_key)
+zapier = ZapierNLAWrapper(zapier_nla_api_key=zapier_api_key)
 toolkit = ZapierToolkit.from_zapier_nla_wrapper(zapier)
 agent = initialize_agent(
-    toolkit.get_tools(), llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True
+    toolkit.get_tools(), 
+    llm=llm, 
+    agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, 
+    verbose=True
 )
 
-
 agent.run(
-    "Send email to kevinkirui1340@gmail.com with a random joke"
+    "Summarize the last email I received regarding Silicon Valley Bank. Send the summary to the #test-zapier channel in slack."
 )
